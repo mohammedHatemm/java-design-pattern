45dd30d88752403a6a404a55ead4b8927cdf208f
mohamed elsherif <mohamed.hatem97825@gmail.com>
2026-02-13 10:14:24 +0200

    the first fill in bad paractice in interface principle when you add or force to be more function in the interface

---
+++ I_InterfaceSegregation/bad/Worker.java
@@ -0,0 +1,12 @@
+package I_InterfaceSegregation.bad;
+
+
+interface Worker{
+    void work();
+    void eat();
+    void sleep();
+    void code();
+    void attendMeeting();
+    void writeReport();
+
+}
